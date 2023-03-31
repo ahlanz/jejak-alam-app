3cd5c86b8025c7e68ec7c61f1240a195f2d91314
ahlanz <ahlangaffar@gmail.com>
2023-03-31 23:24:10 +0700

    feat: add theme aplication jejak alam

---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+int defaultMargin = 30;
+
+Color primaryColor = const Color(0xff8BFF17);
+Color bgcolor1 = const Color(0xffFFFFFF);
+Color bgcolor2 = const Color(0xff1E90FF);
+Color bgcolor3 = const Color(0xff8470FF);
+Color primaryTextColor = const Color(0xff333333);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
