cad8927365e55c822f1ca8a9f2bb0ca2f10e2b14
ahlanz <ahlangaffar@gmail.com>
2023-04-02 22:57:29 +0700

    chore: update code theme and depedencies

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-int defaultMargin = 30;
+double defaultMargin = 30.0;
 
 Color primaryColor = const Color(0xff8BFF17);
 Color bgcolor1 = const Color(0xffFFFFFF);
 Color bgcolor2 = const Color(0xff1E90FF);
 Color bgcolor3 = const Color(0xff8470FF);
 Color primaryTextColor = const Color(0xff333333);
+Color secondaryTextColor = const Color(0xff0075FF);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryTextColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight medium = FontWeight.w500;
